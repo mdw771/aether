6c3505209bd7599654c73a92c87b36bbc4d44996
Ming Du <mingdu@anl.gov>
2025-04-03 15:53:25 -0500

    REFACTOR: `merge calculate_physical_guidance_optimal_z` and `_x`

--- firefly/reconstructors/guided_sampling.py
+++ firefly/reconstructors/guided_sampling.py
@@ -875,10 +875,9 @@ class GuidedLatentDiffusionReconstructor(GuidedDiffusionReconstructor):
             score = self.remove_score_function_outliers(score)
             z_t = z_t - self.options.physical_guidance_scale * score
         elif self.options.physical_guidance_method == api.enums.PhysicalGuidanceMethods.RESAMPLE:
-            if self.options.resample_options.optimize_against_latent:
-                z_0_hat = self.calculate_physical_guidance_optimal_z(z_0_hat)
-            else:
-                z_0_hat = self.calculate_physical_guidance_optimal_x(z_0_hat)
+            z_0_hat = self.calculate_physical_guidance_optimization(
+                z_0_hat, optimize_against_latent=self.options.resample_options.optimize_against_latent
+            )
             z_t = self.stochastic_resample(self.current_denoise_step + 1, z_0_hat, z_t)
         else:
             raise ValueError(f"Invalid physical guidance method: {self.options.physical_guidance_method}")
@@ -1124,13 +1123,18 @@ class GuidedLatentDiffusionReconstructor(GuidedDiffusionReconstructor):
             score[:, c, ...] = score[:, c, ...] * (abs_score_channel < q)
         return score
     
-    def calculate_physical_guidance_optimal_z(self, z: torch.Tensor):
-        """Compute the optimal latent code that minimizes the physical loss.
+    def calculate_physical_guidance_optimization(self, z: torch.Tensor, optimize_against_latent: bool = False):
+        """Optimize the image or latent to minimize the physics negative log-likelihood.
         
         Parameters
         ----------
         z: torch.Tensor
             The estimated latent at time step 0.
+        optimize_against_latent: bool
+            If True, optimization is done directly against the latent code, 
+            with backpropagation running through the decoder. Otherwise, the
+            image is decoded, and the optimization is done against the image,
+            with backpropagation running through the encoder.
             
         Returns
         -------
@@ -1140,75 +1144,32 @@ class GuidedLatentDiffusionReconstructor(GuidedDiffusionReconstructor):
         if z.shape[0] != 1:
             raise ValueError("The length of the batch dimension of z_t must be 1.")
         
-        z_optimizer = pcmaps.get_optimizer_by_enum(
-            self.options.resample_options.optimizer
-        )(
-            [z], lr=self.options.resample_options.step_size
-        )
-        
-        with torch.enable_grad():
-            z = z.requires_grad_(True)
-            for i in range(self.options.resample_options.num_z_optimization_epochs):
-                epoch_loss = 0
-                for batch_data in self.dataloader:
-                    z.grad = None
-                    decoded_image = self.decode_latent(z)
-                    o_hat = self.image_to_object(decoded_image)
-                    self.set_object_data_to_forward_model(o_hat)
-                    
-                    input_data, y_true = self.prepare_batch_data(batch_data)
-                    y_pred = self.forward_model(*input_data)
-                    batch_loss = self.loss_function(
-                        y_pred[:, self.dataset.valid_pixel_mask], y_true[:, self.dataset.valid_pixel_mask]
-                    )
-                    if self.options.resample_options.frequency_loss_weight > 0:
-                        frequency_loss = self.frequency_loss(o_hat)
-                        batch_loss = batch_loss + self.options.resample_options.frequency_loss_weight * frequency_loss
-                    batch_loss.backward(retain_graph=True)
-                    z_optimizer.step()
-                    self.step_all_optimizers()
-                    self.forward_model.zero_grad()
-                    self.loss_tracker.update_batch_loss_with_value(batch_loss.item())
-                self.loss_tracker.conclude_epoch()
-                self.loss_tracker.print_latest()
-        
-        z = z.detach()
-        return z
-    
-    def calculate_physical_guidance_optimal_x(self, z: torch.Tensor):
-        """Decode the latent to pixel space, and compute the optimal pixel-space 
-        image that minimizes the physical loss, then encode it back to latent space.
-        
-        Parameters
-        ----------
-        z: torch.Tensor
-            The estimated latent at time step 0.
-            
-        Returns
-        -------
-        torch.Tensor
-            The resampled latent code.
-        """
-        if z.shape[0] != 1:
-            raise ValueError("The length of the batch dimension of z_t must be 1.")
-        
-        x = self.decode_latent(z)
+        if not optimize_against_latent:
+            x = self.decode_latent(z)
         
         optimizer = pcmaps.get_optimizer_by_enum(
             self.options.resample_options.optimizer
         )(
-            [x], lr=self.options.resample_options.step_size
+            [z if optimize_against_latent else x], lr=self.options.resample_options.step_size
         )
         
         with torch.enable_grad():
-            x = x.requires_grad_(True)
+            if not optimize_against_latent:
+                x = x.requires_grad_(True)
+            else:
+                z = z.requires_grad_(True)
+            
             for i in range(self.options.resample_options.num_z_optimization_epochs):
-                epoch_loss = 0
                 for batch_data in self.dataloader:
                     # self.forward_model.zero_grad() does not zero gradients of x.
-                    x.grad = None
-                    
-                    o_hat = self.image_to_object(x)
+                    if not optimize_against_latent:
+                        x.grad = None
+                        decoded_image = x
+                    else:
+                        z.grad = None
+                        decoded_image = self.decode_latent(z)
+
+                    o_hat = self.image_to_object(decoded_image)
                     self.set_object_data_to_forward_model(o_hat)
                     
                     input_data, y_true = self.prepare_batch_data(batch_data)
@@ -1227,8 +1188,11 @@ class GuidedLatentDiffusionReconstructor(GuidedDiffusionReconstructor):
                 self.loss_tracker.conclude_epoch()
                 self.loss_tracker.print_latest()
         
-        x = x.detach()
-        z = self.encode_image(x)
+        if not optimize_against_latent:
+            x = x.detach()
+            z = self.encode_image(x)
+        else:
+            z = z.detach()
         return z
     
     def frequency_loss(self, o_hat: torch.Tensor):
